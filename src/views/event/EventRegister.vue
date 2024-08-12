91430545804a8c307c3a8134c694815dff6d7330
Antonio Jurić <juricantonio18@gmail.com>
2024-08-12 17:01:41 +0200

    Added event register layout

--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { useRouter } from "vue-router";
+
 defineProps(["event"]);
+
+const router = useRouter();
+
+const onRegister = () => {
+  router.push({ name: "EventDetails" });
+};
 </script>
 
 <template>
   <div v-if="event" class="text-center">
     <p class="mb-10">Register for event here.</p>
+    <button @click="onRegister" class="bg-emerald-300 py-2 px-4 hover:shadow-lg">Register Me!</button>
   </div>
 </template>
