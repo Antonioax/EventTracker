1d10a5e8fdb38a015c9e53f7cd8a8796d84b3bf2
Antonio Jurić <juricantonio18@gmail.com>
2024-08-14 20:26:00 +0200

    Added new tests

--- src/views/__tests__/EventList.spec.js
+++ src/views/__tests__/EventList.spec.js
@@ -3,7 +3,15 @@ import { beforeEach, describe, expect, it, vi } from "vitest";
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import EventList from "../EventList.vue";
-import NetworkError from "../NetworkError.vue";
+import { routes } from "@/router/routes";
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService";
+
+vi.mock("@/services/EventService", () => ({
+  default: {
+    getEvents: vi.fn()
+  }
+}));
 
 const mockRouter = {
   push: vi.fn()
@@ -15,19 +23,16 @@ describe("EventList.vue", () => {
   beforeEach(() => {
     const router = createRouter({
       history: createMemoryHistory(),
-      routes: [
-        {
-          path: "/network-error",
-          name: "NetworkError",
-          component: NetworkError
-        }
-      ]
+      routes: routes
     });
     wrapper = mount(EventList, {
       global: {
         plugins: [router],
         mocks: {
           $router: mockRouter
+        },
+        stubs: {
+          EventCard: true
         }
       }
     });
@@ -35,5 +40,19 @@ describe("EventList.vue", () => {
 
   it("should mount component", () => {
     expect(wrapper).toBeTruthy();
+  });
+
+  it("should render EventCard components for each event", async () => {
+    const mockEvents = [{ id: 1 }, { id: 2 }];
+
+    EventService.getEvents.mockResolvedValue({
+      data: mockEvents,
+      headers: { "x-total-count": 2 }
+    });
+
+    await wrapper.vm.fetchEvents();
+
+    const eventCards = wrapper.findAllComponents(EventCard);
+    expect(eventCards.length).toBe(mockEvents.length);
   });
 });
